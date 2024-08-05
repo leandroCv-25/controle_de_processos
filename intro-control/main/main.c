26228017b78b9f3076066691a8fc1c3b252f13c8
Leandro Campos Vargas <leandrocamposvargas@gmail.com>
2024-08-05 17:03:34 -0300

    Initial Commit

--- intro-control/main/main.c
+++ intro-control/main/main.c
@@ -7,62 +7,51 @@
 
 #include "driver/gpio.h"
 
-#include "servo_motor_drive.h"
+#include "stepper_motor_drive.h"
 
-
-static const char *TAG = "Main - PWM MOTOR BDC";
+static const char *TAG = "Main - Stepper motor";
 
 void app_main(void)
 {
-    static servo_motor_control_context_t motor_ctrl_ctx = {
-        .pcnt_encoder = NULL,
-        .pulses_per_rotation = 1632,
-        .controlData = // Criando os dados de controle para que sejam mostrados
-        {
-            .kp = 0.3,
-            .ki = 0.2,
-            .kd = 0.1,
-            .error = 0,
-            .output_control = 0,
-        },
+    static stepper_motor_control_context_t motor_ctrl_ctx = {
+        .step_gpio = 18,
+        .diretion_gpio = 19,
+        .pulses_per_rotation = 200,
         .size_gear = 13,
     };
 
-
     gpio_num_t homeSensor = 34;
     esp_rom_gpio_pad_select_gpio(homeSensor);
-	gpio_set_direction(homeSensor, GPIO_MODE_INPUT);
+    gpio_set_direction(homeSensor, GPIO_MODE_INPUT);
 
-
-    motor_drive_config(&motor_ctrl_ctx, 22, 23, 33, 32, 0,homeSensor);
+    stepper_motor_drive_config(&motor_ctrl_ctx, homeSensor);
 
     vTaskDelay(pdMS_TO_TICKS(1000));
 
-    while (true)
-    {
+    // while (true)
+    // {
 
-        ESP_LOGI(TAG, "Setando Posição 150");
-        set_servo_motor_position(&motor_ctrl_ctx, 150);
-        ESP_LOGI(TAG, "Position %.2f mm", get_servo_motor_position(&motor_ctrl_ctx));
-        ESP_LOGI(TAG, "ERROR %.3f mm", get_servo_motor_error(&motor_ctrl_ctx));
-        ESP_LOGI(TAG, "OUTPUT %.3f", get_servo_motor_control_output(&motor_ctrl_ctx));
-        
-            vTaskDelay(pdMS_TO_TICKS(2000));
-            ESP_LOGI(TAG, "Position %.2f mm", get_servo_motor_position(&motor_ctrl_ctx));
-            ESP_LOGI(TAG, "Velocidade %.2f RPM", get_servo_motor_speed(&motor_ctrl_ctx));
-            ESP_LOGI(TAG, "ERROR %f mm", get_servo_motor_error(&motor_ctrl_ctx));
-            ESP_LOGI(TAG, "OUTPUT %.3f %%\n\n\n", get_servo_motor_control_output(&motor_ctrl_ctx));
-        
+    //     ESP_LOGI(TAG, "Setando Posição 150");
+    //     set_servo_motor_position(&motor_ctrl_ctx, 150);
+    //     ESP_LOGI(TAG, "Position %.2f mm", get_servo_motor_position(&motor_ctrl_ctx));
+    //     ESP_LOGI(TAG, "ERROR %.3f mm", get_servo_motor_error(&motor_ctrl_ctx));
+    //     ESP_LOGI(TAG, "OUTPUT %.3f", get_servo_motor_control_output(&motor_ctrl_ctx));
 
-        for (int i = 150; i > 0; i--)
-        {
-            ESP_LOGI(TAG, "Setando Posição %d", i);
-            set_servo_motor_position(&motor_ctrl_ctx, i);
-            vTaskDelay(pdMS_TO_TICKS(2000));
-            ESP_LOGI(TAG, "Position %.2f mm", get_servo_motor_position(&motor_ctrl_ctx));
-            ESP_LOGI(TAG, "Velocidade %.2f RPM", get_servo_motor_speed(&motor_ctrl_ctx));
-            ESP_LOGI(TAG, "ERROR %f mm", get_servo_motor_error(&motor_ctrl_ctx));
-            ESP_LOGI(TAG, "OUTPUT %.3f %%\n\n\n", get_servo_motor_control_output(&motor_ctrl_ctx));
-        }
-    }
+    //     vTaskDelay(pdMS_TO_TICKS(2000));
+    //     ESP_LOGI(TAG, "Position %.2f mm", get_servo_motor_position(&motor_ctrl_ctx));
+    //     ESP_LOGI(TAG, "Velocidade %.2f RPM", get_servo_motor_speed(&motor_ctrl_ctx));
+    //     ESP_LOGI(TAG, "ERROR %f mm", get_servo_motor_error(&motor_ctrl_ctx));
+    //     ESP_LOGI(TAG, "OUTPUT %.3f %%\n\n\n", get_servo_motor_control_output(&motor_ctrl_ctx));
+
+    //     for (int i = 150; i > 0; i--)
+    //     {
+    //         ESP_LOGI(TAG, "Setando Posição %d", i);
+    //         set_servo_motor_position(&motor_ctrl_ctx, i);
+    //         vTaskDelay(pdMS_TO_TICKS(2000));
+    //         ESP_LOGI(TAG, "Position %.2f mm", get_servo_motor_position(&motor_ctrl_ctx));
+    //         ESP_LOGI(TAG, "Velocidade %.2f RPM", get_servo_motor_speed(&motor_ctrl_ctx));
+    //         ESP_LOGI(TAG, "ERROR %f mm", get_servo_motor_error(&motor_ctrl_ctx));
+    //         ESP_LOGI(TAG, "OUTPUT %.3f %%\n\n\n", get_servo_motor_control_output(&motor_ctrl_ctx));
+    //     }
+    // }
 }
---
+++ intro-control/main/stepper_motor_drive.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+
+#include "driver/gpio.h"
+
+#include "stepper_motor_drive.h"
+
+#define M_PI 3.14159265358979323846
+
+static const char *TAG = "Motor Step";
+
+void set_step_motor_position(stepper_motor_control_context_t *motor_ctrl_ctx, float new_position)
+{
+    motor_ctrl_ctx->expect_position = (int)(motor_ctrl_ctx->pulses_per_rotation * new_position / (M_PI * (motor_ctrl_ctx->size_gear)));
+}
+
+float get_step_motor_position(stepper_motor_control_context_t *motor_ctrl_ctx)
+{
+
+    return ((float)(motor_ctrl_ctx->report_pulses) / (motor_ctrl_ctx->pulses_per_rotation)) * M_PI * (motor_ctrl_ctx->size_gear);
+}
+
+// float get_step_motor_speed(stepper_motor_control_context_t *motor_ctrl_ctx)
+// {
+//     return (motor_ctrl_ctx->delta_pulses) * 60000 / ((motor_ctrl_ctx->pulses_per_rotation) * BDC_PID_LOOP_PERIOD_MS);
+// }
+
+// float get_step_motor_error(stepper_motor_control_context_t *motor_ctrl_ctx)
+// {
+//     return ((float)(motor_ctrl_ctx->controlData.error) / (motor_ctrl_ctx->pulses_per_rotation)) * M_PI *  (motor_ctrl_ctx->size_gear);
+// }
+
+// float get_step_motor_control_output(stepper_motor_control_context_t *motor_ctrl_ctx)
+// {
+//     return 100 * (motor_ctrl_ctx->controlData.output_control) / BDC_MCPWM_DUTY_TICK_MAX;
+// }
+
+void stepper_motor_drive_config(stepper_motor_control_context_t *motor_ctrl_ctx, int home_sensor)
+{
+
+   
+    motor_ctrl_ctx->expect_position = 0;
+
+    ESP_LOGI(TAG, "Criando o STEP motor");
+    esp_rom_gpio_pad_select_gpio(motor_ctrl_ctx->step_gpio);
+    gpio_set_direction(motor_ctrl_ctx->step_gpio, GPIO_MODE_OUTPUT);
+    esp_rom_gpio_pad_select_gpio(motor_ctrl_ctx->diretion_gpio);
+    gpio_set_direction(motor_ctrl_ctx->diretion_gpio, GPIO_MODE_OUTPUT);
+
+    // Homing
+    gpio_set_level(motor_ctrl_ctx->diretion_gpio, true);
+    for (int i = 0; i < 15; i++)
+    {
+        gpio_set_level(motor_ctrl_ctx->step_gpio, true);
+        vTaskDelay(pdMS_TO_TICKS(10));
+        gpio_set_level(motor_ctrl_ctx->step_gpio, false);
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+    gpio_set_level(motor_ctrl_ctx->diretion_gpio, false);
+    while (gpio_get_level(home_sensor))
+    {
+        gpio_set_level(motor_ctrl_ctx->step_gpio, true);
+        vTaskDelay(pdMS_TO_TICKS(10));
+        gpio_set_level(motor_ctrl_ctx->step_gpio, false);
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+
+    motor_ctrl_ctx->expect_position = 0;
+    motor_ctrl_ctx->report_pulses = 0;
+}
